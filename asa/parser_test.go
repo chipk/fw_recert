47287c4de5bb96ffc409bd2d846216876902308a
Chip Keefer <chipkeefer@gmail.com>
2018-10-23 00:28:28 -0400

    move test data directory; update parse_test file open and close

--- asa/parser_test.go
+++ asa/parser_test.go
@@ -2,17 +2,19 @@ package asa
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 )
 
 func TestParse(t *testing.T) {
 
-	r, err := os.Open("Cisco-ASA5506-config.txt")
+	r, err := os.Open(filepath.Join("test_data", "Cisco-ASA5506-config.txt"))
 
 	if err != nil {
 		t.Fatal(err)
 		t.FailNow()
 	}
+	defer r.Close()
 
 	config := Parse(r)
 
